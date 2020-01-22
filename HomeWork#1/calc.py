181cdba3bc6a7faf7c663aaed187fa1ca48aa6b9
OskarWise <OskarWise@gmail.com>
2020-01-22 09:39:01 +0700

    1

--- HomeWork#1/calc.py
+++ HomeWork#1/calc.py
@@ -1,29 +1,9 @@
 import time
 import operator
-import random
-
-fault = 0
-creature = ["mortal", "human", "worm", "weakling", "creature"]
 
 
 def invalid_command():
-    global fault
-    fault += 1
-    message = f"I don't understand you, {random.choice(creature)}. But I'm giving you another chance."
-    if fault < 3:
-        print(message)
-    elif fault == 3:
-        print(message + " Don't try your luck.")
-    elif fault == 5:
-        print(message + " Our game is dragging on.")
-    elif fault == 7:
-        print("I'M ANGRY!")
-        time.sleep(1)
-        for i in range(10):
-            print(get_operator(random.randint(0, 666), random.randint(0, 666), "+"))
-        time.sleep(1)
-        print(f"You've had enough, {random.choice(creature)}")
-        fault = 0
+    print("Invalid value")
 
 
 def get_operator(number_1, number_2, operation):
@@ -33,43 +13,40 @@ def get_operator(number_1, number_2, operation):
             '-': operator.sub(number_1, number_2),
             '*': operator.mul(number_1, number_2),
             '/': operator.truediv(number_1, number_2),
-            '%': operator.mod(number_1, number_2),
             '**': operator.pow(number_1, number_2),
-            '//': operator.floordiv(number_1, number_2)
+            '//': operator.floordiv(number_1, number_2),
+            '%': operator.mod(number_1, number_2)
         }[operation]
 
     except ZeroDivisionError:
-        print(f"Even my powers are not enough to divide by 0, {random.choice(creature)}")
+        print("Cannot be divided by 0")
     except KeyError:
         invalid_command()
 
 
 def calc(answer):
-    global fault
     if answer.upper() == "Y":
         try:
-            number_1, number_2 = int(input(f"Enter first number,{random.choice(creature)} ")), int(
-                input(f"Enter second number,{random.choice(creature)} "))
-            operation = input("Choose operation: +, -, *, /, %, **, //. And then hope to get out of here alive. ")
+            number_1, number_2 = int(input("Enter first number ")), int(input("Enter second number "))
+            operation = input("Choose operation: +, -, *, /, **, //, % ")
             result = get_operator(number_1, number_2, operation)
             if result is not None:
-                print(f"BEHOLD MY POWER!\n{number_1} {operation} {number_2} = {result}")
-                fault = 0
+                print(f"Result is \n{number_1} {operation} {number_2} = {result}")
 
         except ValueError:
             invalid_command()
 
     elif answer.upper() == "N":
-        print("You underestimate my POWER!")
-        time.sleep(3)
+        print("Exit")
+        time.sleep(1)
         exit()
 
     elif answer.upper != "N" or "Y":
         invalid_command()
 
-
-answer = str(input(f"Hi {random.choice(creature)}!Do you want to see my GREAT power of my GREAT calculation?(Y/N) "))
-calc(answer)
-while True:
-    answer_again = str(input("Do you want to see my GREAT power again?(Y/N) "))
-    calc(answer_again)
+if __name__ == "__main__":
+    answer = str(input("Do you want calculation?(Y/N) "))
+    calc(answer)
+    while True:
+        answer_again = str(input("Do you want calculation again?(Y/N) "))
+        calc(answer_again)
--- HomeWork#2/redactor.py
+++ HomeWork#2/redactor.py
@@ -1,14 +1,25 @@
 import os
 import re
 
-with open("data_new.txt", "w"): pass
-with open("data.txt","r") as data:
-    for line in data:
-        line = line.replace(".", "")
-        line = line.replace(",", "")
-        line = line.lower()
-        while "  " in line:
-            line = line.replace("  ", " ")
-        with open ("data_new.txt", "a") as data_new:
+def redactor_1(line):
+    line = line.replace(".", "")
+    line = line.replace(".", "")
+    return line
+
+def redactor_2(line):
+    line = line.lower()
+    return line
+
+def redactor_3(line):
+    line = line.replace("  ", " ")
+    return line
+
+if __name__ == "__main__":
+    with open("data_new.txt", "w"): pass
+    with open("data.txt","r") as data, open ("data_new.txt", "a") as data_new:
+        for line in data:
+            line = redactor_1(line)
+            line = redactor_2(line)
+            line = redactor_3(line)
             data_new.write(line)
 
