6a64514cb8d93dbbc90e797b16d4cec20070472d
ManyManyEyes <50905215+ManyManyEyes@users.noreply.github.com>
2020-02-06 22:30:41 +0700

    Update validator.py

--- HomeWork#4/validator.py
+++ HomeWork#4/validator.py
@@ -9,19 +9,25 @@ def redactor(line):
         .split(",")
 
 
-def validator(userlist):
+def validator_id(userlist):
     if not re.match('^[1-9][0-9]*$', userlist[0]):
         userlist[0] = str(None)
+def validator_name(userlist):
     if not re.match('^[A-Z|a-z][a-z]+$', userlist[1]):
         userlist[1] = str(None)
+def validator_age(userlist):        
     if not re.match('^[1-9][0-9]*$', userlist[2]):
         userlist[2] = str(None)
+def validator_phone(userlist):
     if not re.match('^[+]*[0-9]{0,11}$', userlist[3]):
         userlist[3] = str(None)
+def validator_mail(userlist):
     if not re.match('^[a-z0-9]+@[a-z]+.[a-z]+$', userlist[4].lower()):
         userlist[4] = str(None)
+def validator_role(userlist):
     if not re.match('^(admin|user|guest)$', userlist[5]):
         userlist[5] = str(None)
+def validator_status(userlist):
     if not re.match('^(active|inactive)$', userlist[6]):
         userlist[6] = str(None)
 
@@ -43,7 +49,13 @@ if __name__ == "__main__":
         persons = []
         for line in data:
             line = redactor(line)
-            validator(line)
+            validator_id(line)
+            validator_name(line)
+            validator_age(line)
+            validator_phone(line)
+            validator_mail(line)
+            validator_role(line)
+            validator_status(line)
             person = constructor(pointer_line, line)
             persons.append(person)
         print(persons)
